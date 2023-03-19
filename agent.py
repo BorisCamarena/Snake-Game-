81629e3fd5b942a113ff6b2d2a7a0686a36b250f
I-va123 <rcamarena@apaciap.org.mx>
2023-03-19 03:32:39 -0600

    Snake Game

---
+++ agent.py
@@ -0,0 +1,197 @@
+
+# Aqui importamos las librerias previamente instaladas en la consola.
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
+
+# Linea importante en el codigo.
+MAX_MEMORIA = 100_000
+# Definimos el tamañano de la cola.
+
+T_COLA = 1000
+LR = 0.001
+
+# Creamos la clase agente.
+
+class Agente:
+
+	# Aquì se tiene el constructor:
+	# modelo - red neuronal.
+	# Optimizador. Mas adelante veremos lo curioso en el gradiente.
+
+    def __init__(self):
+	    # Se omiten algunas traducciones, por que son inmediatas.
+        self.n_games = 0
+        self.epsilon = 0 # juegos al azar.
+        self.gamma = 0.9 # tasa de descuento.
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        # Parte importante en el còdigo, por que
+        # se crea una pila con el popleft().
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+# Creamos nuestra Clase estado - Agente.
+
+    def ob_estado(self, juego):
+	    # definimos la cabeza de la vibora.
+        head = juego.snake[0]
+
+        # Definimos el tamaño del pixel 20x20
+        
+        point_l = Punto(head.x - 20, head.y)
+        point_r = Punto(head.x + 20, head.y)
+        point_u = Punto(head.x, head.y - 20)
+        point_d = Punto(head.x, head.y + 20)
+
+        # Definimos las direcciones, que es super
+        # importante para definir el comportamiento
+        # de la viborita.
+        
+        dir_l = juego.direction == Direccion.LEFT
+        dir_r = juego.direction == Direccion.RIGHT
+        dir_u = juego.direction == Direccion.UP
+        dir_d = juego.direction == Direccion.DOWN
+
+        state = [
+
+		# Importancia de definir las direcciones.
+		
+            # Peligro enfrente.
+            (dir_r and juego.is_collision(point_r)) or 
+            (dir_l and juego.is_collision(point_l)) or 
+            (dir_u and juego.is_collision(point_u)) or 
+            (dir_d and juego.is_collision(point_d)),
+
+            # Peligro a la derecha.
+            (dir_u and juego.is_collision(point_r)) or 
+            (dir_d and juego.is_collision(point_l)) or 
+            (dir_l and juego.is_collision(point_u)) or 
+            (dir_r and juego.is_collision(point_d)),
+
+            # Peligro a la inzquierda.
+	    
+            (dir_d and juego.is_collision(point_r)) or 
+            (dir_u and juego.is_collision(point_l)) or 
+            (dir_r and juego.is_collision(point_u)) or 
+            (dir_l and juego.is_collision(point_d)),
+
+	    # IMPORTANTE - Direccion de movimiento.
+            
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            # Comida - posicion.
+
+            juego.food.x < juego.head.x,  # comida(izquierda)
+            juego.food.x > juego.head.x,  # comida(derecha)
+            juego.food.y < juego.head.y,  # comida(arriba)
+            juego.food.y > juego.head.y  # comida(abajo)
+            ]
+
+        # Aqui lo que hace es estar en enteros ( 0 ò 1 ).
+
+        return np.array(state, dtype=int)
+
+# Introducimos el estado, accion, premio, sig_estado, echo.
+
+    def recordar(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached - si alcanza memoria maxima.
+
+# Entrenamiento de memoria, se trabajara con tuplas ( lista de tuplas ).
+
+    def entrenar_memoria_larga(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, T_COLA) 
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+
+ # Introducimos la funcion analoga - memoria a corto plazo.
+ # Entrenamiento.
+
+    def entrenar_memoria_corta(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+# Funcion super importante. Se deciden acciones.
+# Con enfoque probabilistico ?
+
+    def ob_accion(self, state):
+        # Movimientos al azar ( exploracion - explotacion ).
+        self.epsilon = 80 - self.n_games
+        # Movimiento final.
+        final_move = [0,0,0]
+        # Vamos a generar ennteros 0 - 2.
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            # Dado el estado (R11) genera predict(R3)
+            prediction = self.model(state0)
+            # Como se definio en el if, move genra enteros entre 0 - 2
+            # que es max_predict(R3)
+            move = torch.argmax(prediction).item()
+            # Movimiento final.
+            final_move[move] = 1
+
+            # Decision -> ( vector ) perteneciente a R3 con 0 - 1.
+
+        return final_move
+
+# Creamos la funcion entreamiento.
+
+def entrenar():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agente = Agente()
+    juego = ViboritaInteligente()
+    while True:
+        # estado viejo.
+        state_old = agente.ob_estado(juego)
+
+        # obtener movimiento.
+        final_move = agente.ob_accion(state_old)
+
+        # Mover -> nuevo estado.
+        reward, done, score = juego.play_step(final_move)
+        state_new = agente.ob_estado(juego)
+
+        # entrenar memoria corta.
+        agente.entrenar_memoria_corta(state_old, final_move, reward, state_new, done)
+
+        # recordar
+        agente.recordar(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # Graficamos el resultado con el entrenamiento
+            # de la memoria larga.
+            juego.reset()
+            agente.n_games += 1
+            agente.entrenar_memoria_larga()
+
+            if score > record:
+                record = score
+                agente.model.save()
+
+            print('Juego', agente.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            graficar(plot_scores, plot_mean_scores)
+
+# Funcion principal.
+
+if __name__ == '__main__':
+    entrenar()
---
+++ juego.py
@@ -0,0 +1,206 @@
+
+
+# Importamos librerias.
+
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+# Direcciones de movimiento.
+class Direccion(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+# Coordenadas ( x , y ) -> Punto.
+    
+Punto = namedtuple('Point', 'x, y')
+
+# Colores -  rgb. 
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+BLOCK_SIZE = 20
+SPEED = 20
+
+# Construimos nuestra clases V-IA.
+
+class ViboritaInteligente:
+
+	# Constructor.
+    
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Viborita Inteligente.')
+        self.clock = pygame.time.Clock()
+        
+        # init game state - resetear el juego.
+
+        def reset(self):
+		
+        self.direction = Direccion.RIGHT
+        
+        self.head = Punto(self.w/2, self.h/2)
+        self.snake = [self.head, 
+                      Punto(self.head.x-BLOCK_SIZE, self.head.y),
+                      Punto(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration=0
+        # Premio.
+        self.reward=0
+
+        # Posicion de la comida.
+        
+    def _place_food(self):
+        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
+        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        self.food = Punto(x, y)
+        if self.food in self.snake:
+            self._place_food()
+
+            # Funcion jugar a un pasp.
+        
+    def play_step(self):
+
+	    self.frame_iteration +=1
+        # 1. collect user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.direction = Direction.LEFT
+                elif event.key == pygame.K_RIGHT:
+                    self.direction = Direction.RIGHT
+                elif event.key == pygame.K_UP:
+                    self.direction = Direction.UP
+                elif event.key == pygame.K_DOWN:
+                    self.direction = Direction.DOWN
+        
+        # 2. movimiento.
+        self._move(action) # update the head
+        self.snake.insert(0, self.head)
+        
+        # 3. checar si perdiò.
+        self.reward=0
+        game_over = False
+        if self._is_collision()or self.frame_iteration>100*len(self.snake):
+            game_over = True
+            # Premio +- 10.
+            self.reward = -10 
+            return  self.reward ,game_over, self.score
+            
+        # 4. Nuevo lugar de comida - muevete.
+        if self.head == self.food:
+            self.score += 1
+            # Premio de comida +10
+            self.reward +=10
+            self._place_food()
+        else:
+            self.snake.pop()
+
+            # Penalizar.
+        if self.frame_iteration > 20:
+
+		self.reward -= int(0.001*self.frame_iteration)
+      
+        # 5. update ui and clock
+        self._update_ui()
+        self.clock.tick(SPEED)
+        # 6. Perdiste, regresa el juego y record a 0.
+        return game_over, self.score
+
+# Funcion importantisima - Colisiones
+    
+    def _is_collision(self, pt = None):
+
+	if pt is None:
+		pt=self.head
+        # hits boundary
+        if pt.x > self.w - BLOCK_SIZE or pt.x<0 or pt.y>self.h - BLOCK_SIZE or pt.y < 0:
+            return True
+        # hits itself
+        if pt in self.snake[1:]:
+            return True
+        
+        return False
+
+# Cuadro de pantalla.
+        
+    def _update_ui(self):
+        self.display.fill(BLACK)
+        
+        for pt in self.snake:
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+            
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0, 0])
+        pygame.display.flip()
+
+        # Funcion moverse.
+        
+    def _move(self, action):
+
+	clock_wise = [ Direccion.RIGHT,Direccion.DOWN,Direccion.LEFT,Direccion.UP]
+	idx = clock_wise.index(self.direction)
+
+	if np.array_equal(action,[1,0,0]):
+		new_dir = clock_wise[ idx] # no cambia.
+	elif np.array_equal(action,[ 0,1,0]):
+		next_idx = (idx +1)%4
+		# Direccion r -> d -> l -> u 
+		new_dir = clock_wise[ next_idx]
+	else: #[ 0,0,1]
+
+		next_idx = (idx-1)%4
+		# Direccion r -> u -> l -> d
+		new_dir= clock_wise[ next_idx]
+
+	self.direction = new.dir
+
+	x = self.head.x
+	y = self.head.y
+
+	if direction == Direction.RIGHT:
+            x += BLOCK_SIZE
+        elif direction == Direction.LEFT:
+            x -= BLOCK_SIZE
+        elif direction == Direction.DOWN:
+            y += BLOCK_SIZE
+        elif direction == Direction.UP:
+            y -= BLOCK_SIZE
+            
+        self.head = Punto(x, y)
+            
+
+#if __name__ == '__main__':
+ #   game = SnakeGame()
+    
+    # game loop
+  #  while True:
+   #     game_over, score = game.play_step()
+        
+       # if game_over == True:
+        #    break
+        
+    #print('Final Score', score)
+        
+        
+    #pygame.quit()
