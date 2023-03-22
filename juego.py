5c23fb4703f75397c9ab4f33098ffefc2ee1f1ca
BorisCamarena <128287239+BorisCamarena@users.noreply.github.com>
2023-03-22 16:59:43 -0600

    Update juego.py

--- juego.py
+++ juego.py
@@ -75,7 +75,7 @@ class ViboritaInteligente:
             # Funcion jugar a un pasp.
         
     def play_step(self,action):
-	self.frame_iteration += 1
+	self.frame_iteration+=1
         # 1. collect user input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
