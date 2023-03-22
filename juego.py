2480f2149419c45fbd096d2af780bc21d4d0bb06
BorisCamarena <128287239+BorisCamarena@users.noreply.github.com>
2023-03-22 17:29:21 -0600

    Update juego.py

--- juego.py
+++ juego.py
@@ -77,7 +77,7 @@ class ViboritaInteligente:
     def play_step(self,action):
 	   self.frame_iteration+=1
         # 1. collect user input
-           for event in pygame.event.get():
+          for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
